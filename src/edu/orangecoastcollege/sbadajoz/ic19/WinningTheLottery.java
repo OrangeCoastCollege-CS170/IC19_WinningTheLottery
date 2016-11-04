0d49571ba83ad72e5a6144ebde4b7b63b38f456e
Badajoz <sbadajoz@student.cccd.edu>
2016-11-04 13:37:17 -0700

    added header

--- src/edu/orangecoastcollege/sbadajoz/ic19/WinningTheLottery.java
+++ src/edu/orangecoastcollege/sbadajoz/ic19/WinningTheLottery.java
@@ -1,7 +1,14 @@
 package edu.orangecoastcollege.sbadajoz.ic19;
 
+import java.text.NumberFormat;
 import java.util.Random;
+/*
+Badajoz, Seve
+CS A170
+November 4, 2016
 
+IC19
+*/
 public class WinningTheLottery {
     public static void randomlyAssignNumbers(int[]array, Random ran) {       
         for(int i = 0 ; i < array.length; i++) {
@@ -21,6 +28,8 @@ public class WinningTheLottery {
     public static void main(String[] args) {
         int[] winningNum = new int[5];
         int[] guessedNum = new int[5];
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        
         boolean found = false;
         int counter = 0;
         Random ran = new Random();
@@ -38,6 +47,6 @@ public class WinningTheLottery {
             found = howManyCorrect(winningNum, guessedNum) == 5;
             counter++;
         }
-        System.out.println("\n\nAfter spending $" + counter + ".00, you won Fantasy5!  5 out of 5 lottery numbers were correct.  You win $75,000");
+        System.out.println("\n\nAfter spending " + currency.format(counter) + ", you won Fantasy5!  5 out of 5 lottery numbers were correct.  You win $75,000");
     }
 }
